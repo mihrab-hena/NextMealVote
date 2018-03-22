71107ce2759da0d9f0415288067e3c87ea5ee1c7
mihrab-hena <mihrabhena@gmail.com>
2018-03-22 19:17:45 +0100

    property changed from District to State

--- NextMealVote/Models/Address.cs
+++ NextMealVote/Models/Address.cs
@@ -12,7 +12,7 @@ namespace NextMealVote.Models
         public string StreetName { get; set; }
         public string PostCode { get; set; }
         public string City { get; set; }
-        public string District { get; set; }
+        public string State { get; set; }
         public string Country { get; set; }
         public string ResidentialStatus { get; set; }
 
